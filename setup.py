7f5fdb048fd3ced8f4e64b8c2c2b05306055aca9
neffertitijf <jocelynneff@Jocelyns-MacBook-Pro-2.local>
2014-06-24 15:33:14 -0700

    Merge branch 'master' of https://github.com/infoscout/pymstr
    Conflicts:
    	setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 
 from setuptools import find_packages, setup
 
+tests_require=['mox==0.5.3']
+if platform.python_version() < '2.7':
+    tests_require.append("unittest2")
 setup(name='py-mstr',
     packages=find_packages(),  
     description = 'Python API for Microstrategy Web Tasks',
@@ -9,8 +12,6 @@ setup(name='py-mstr',
         'pyquery==1.2.8',
         'requests==2.3.0',
     ],
-    tests_require=[
-        'mox==0.5.3',
-    ],
-    test_suite= "tests.run_tests",
+    tests_require=tests_require,
+    test_suite="tests.run_tests",
 )
