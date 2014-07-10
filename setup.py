4aeb7a3e31f8fa9424f153aa7b56da10c1a96888
neffertitijf <jocelynneff@Jocelyns-MacBook-Pro-2.local>
2014-07-10 11:38:08 -0700

    v1.0.0 pypi update

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@
 from setuptools import find_packages, setup
 
 tests_require=['mox==0.5.3', 'discover']
-setup(name='py-mstr',
+setup(
+    name='py-mstr',
     packages=find_packages(),  
     description = 'Python API for Microstrategy Web Tasks',
     url = 'http://github.com/infoscout/py-mstr',
@@ -10,6 +11,7 @@ setup(name='py-mstr',
         'pyquery==1.2.8',
         'requests==2.3.0',
     ],
+    download_url = 'https://github.com/infoscout/py-mstr/tarball/v0.1.0',
     tests_require=tests_require,
     test_suite="tests.get_tests",
 )
