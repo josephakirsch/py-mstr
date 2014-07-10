8c1c0333c7c0ca0aa30fb3e2235aa8e33260ed6a
neffertitijf <jocelynneff@Jocelyns-MacBook-Pro-2.local>
2014-07-10 12:00:02 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 
 from setuptools import find_packages, setup
 
+with open('py_mstr/__init__.py', 'rb') as f:
+    version = str(re.search('__version__ = "(.+?)"', f.read().decode('utf-8')).group(1))
+
 tests_require=['mox==0.5.3', 'discover']
 setup(
     name='py-mstr',
+    version=version,
     packages=find_packages(),  
     description = 'Python API for Microstrategy Web Tasks',
     url = 'http://github.com/infoscout/py-mstr',
+    author='InfoScout',
+    author_email='oss@infoscoutinc.com',
+    lincense='MIT',
     install_requires=[
         'pyquery==1.2.8',
         'requests==2.3.0',
@@ -14,4 +21,11 @@ setup(
     download_url = 'https://github.com/infoscout/py-mstr/tarball/v0.1.0',
     tests_require=tests_require,
     test_suite="tests.get_tests",
+
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries',
+    ],
 )
